7c5d9a6315b9cadbbb88c83e46369c47df2485fe
Tamás Gulácsi <tamas@gulacsi.eu>
2026-02-27 11:53:08 +0100

    kiota-generated rest api

--- rest/gen.go
+++ rest/gen.go
@@ -1,4 +1,7 @@
 package dokuwiki
 
-//go:generate go tool openapi-generator-cli generate --package-name dokuwiki --minimal-update -i dokuwiki.json -g go
-//go:generate rm -f go.*
+// go : generate go tool openapi-generator-cli generate --package-name dokuwiki --minimal-update -i dokuwiki.json -g go
+// go : generate rm -f go.*
+//
+// nix-shell -p steam-run --run "steam-run openapi-generator-cli kiota -l Go -o dw -n github.com/UNO-SOFT/dokuwiki/rest/dw -d dokuwiki.json"
+//go:generate go tool openapi-generator-cli kiota -l Go -o dw -n github.com/UNO-SOFT/dokuwiki/rest/dw -d dokuwiki.json
